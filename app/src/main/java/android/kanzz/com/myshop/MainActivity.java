5a2f120b1b61a16dbecc3230ec56a9c73faafb53
zyc2621854 <879615001@qq.com>
2018-12-30 17:11:05 +0800

    bug调得都忘记上传之完成toolbar和一半的广告轮滚

--- app/src/main/java/android/kanzz/com/myshop/MainActivity.java
+++ app/src/main/java/android/kanzz/com/myshop/MainActivity.java
@@ -67,10 +67,8 @@ public class MainActivity extends AppCompatActivity {
         View view =mInflater.inflate(R.layout.tab_indicator,null);
          ImageView img=(ImageView)view.findViewById(R.id.icon_tab);
         TextView text=(TextView)view.findViewById(R.id.txt_indicator);
-
         img.setBackgroundResource(tab.getIcon());
         text.setText(tab.getTitle());
-
         return view;
     }
 }
--- app/src/main/java/android/kanzz/com/myshop/fragment/HomeFragment.java
+++ app/src/main/java/android/kanzz/com/myshop/fragment/HomeFragment.java
@@ -2,6 +2,7 @@ package android.kanzz.com.myshop.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,22 +10,55 @@ import android.view.ViewGroup;
 
 import android.kanzz.com.myshop.R;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    private SliderLayout mSliderLayout;
 
-    public HomeFragment() {
-        // Required empty public constructor
+    private PagerIndicator indicator;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_home,container,false);
+        mSliderLayout=(SliderLayout)view.findViewById(R.id.slider);
+
+        indicator=(PagerIndicator)view.findViewById(R.id.custom_indicator);
+
+        initSlider();
+        return view;
     }
 
+    private void initSlider(){
+        TextSliderView textSliderView = new TextSliderView(this.getActivity());
+        textSliderView
+                .description("新品推荐")
+                .image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t2416/102/20949846/13425/a3027ebc/55e6d1b9Ne6fd6d8f.jpg");
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        TextSliderView textSliderView2 = new TextSliderView(this.getActivity());
+        textSliderView
+                .description("时尚男装")
+                .image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1507/64/486775407/55927/d72d78cb/558d2fbaNb3c2f349.jpg");
+
+        TextSliderView textSliderView3 = new TextSliderView(this.getActivity());
+        textSliderView
+                .description("家电秒杀")
+                .image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1363/77/1381395719/60705/ce91ad5c/55dd271aN49efd216.jpg");
+
+        mSliderLayout.addSlider(textSliderView);
+        mSliderLayout.addSlider(textSliderView2);
+        mSliderLayout.addSlider(textSliderView3);
+
+        mSliderLayout.setCustomIndicator(indicator);
+        mSliderLayout.setCustomAnimation(new DescriptionAnimation());
+        mSliderLayout.setPresetTransformer(SliderLayout.Transformer.RotateUp);
+        mSliderLayout.setDuration(3000);
     }
 
 }
